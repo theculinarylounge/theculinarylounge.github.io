25f3154a9a0f43eb830b4d79c4353a03da45d562
Ravi Raja Merugu <rrmerugu@gmail.com>
2019-05-04 01:52:03 +0530

    newsletter assets added

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 
 
-sed -i -- 's/http:\/\/localhost:8000/https:\/\/tcl-newsletter-backend-service.herokuapp.com/g' _site/assets/js/*.js
+sed -i -- 's/http:\/\/localhost:8002/https:\/\/tcl-newsletter-backend-service.herokuapp.com/g' _site/assets/js/*.js
